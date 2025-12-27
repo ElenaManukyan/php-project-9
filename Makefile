1f81f2fc6d32e97ffd3b33253bbad194783737a4
Манукьян Елена Михайловна <e.manukyan@aitiguru.com>
2025-12-27 14:55:20 +0300

    feat: add home page

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@ PORT ?= 8000
 start:
 	PHP_CLI_SERVER_WORKERS=5 php -S 0.0.0.0:$(PORT) -t public
 
+stop:
+	-fuser -k $(PORT)/tcp
+
 install:
 	composer install
 
--- public/index.php
+++ public/index.php
@@ -1,14 +1,16 @@
 <?php
 
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Hello, Hexlet!');
-    return $response;
+$renderer = new PhpRenderer(__DIR__ . '/../templates');
+
+$app->get('/', function ($request, $response) use ($renderer) {
+    return $renderer->render($response, "home.phtml");
 });
 
 $app->run();
