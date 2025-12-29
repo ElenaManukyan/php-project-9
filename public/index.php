a42cc99c9f8bf847358710b0b45fcc70c2f08902
Манукьян Елена Михайловна <e.manukyan@aitiguru.com>
2025-12-29 13:05:29 +0300

    fix: search bug

--- public/index.php
+++ public/index.php
@@ -40,7 +40,7 @@ var_dump($databaseUrl);
 $conStr = sprintf(
     "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
     $databaseUrl['host'],
-    $databaseUrl['port'] ?? '',
+    $databaseUrl['port'] ?? 5432,
     ltrim($databaseUrl['path'], '/'),
     $databaseUrl['user'],
     $databaseUrl['pass']
